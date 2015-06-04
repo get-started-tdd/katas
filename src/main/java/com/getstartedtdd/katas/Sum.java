7e09df0ed6bf52e80bec59d3a5adfafaac5418e9
get-started-tdd <32414154@qq.com>
2015-06-05 00:18:06 +0800

    refactoring => replace while loop with for loop

--- src/main/java/com/getstartedtdd/katas/Sum.java
+++ src/main/java/com/getstartedtdd/katas/Sum.java
@@ -6,10 +6,8 @@ package com.getstartedtdd.katas;
 public class Sum {
     public static int sum(int[] numbers) {
         int result = 0;
-        int index = 0;
-        while (numbers.length > index) {
+        for (int index = 0; numbers.length > index; index++) {
             result += numbers[index];
-            index++;
         }
         return result;
     }
