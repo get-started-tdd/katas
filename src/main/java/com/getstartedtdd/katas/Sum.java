16fe57c8ce6d591d111d9cb39cc48b6c072f9028
get-started-tdd <32414154@qq.com>
2015-06-05 00:10:00 +0800

    $sum with array containing another single number returns the number => by replace constant with variable

--- src/main/java/com/getstartedtdd/katas/Sum.java
+++ src/main/java/com/getstartedtdd/katas/Sum.java
@@ -7,7 +7,7 @@ public class Sum {
     public static int sum(int[] numbers) {
         int result = 0;
         if (numbers.length > 0) {
-            result += 1;
+            result += numbers[0];
         }
         return result;
     }
