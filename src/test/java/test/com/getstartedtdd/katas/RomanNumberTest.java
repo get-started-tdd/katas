48a60bba914f50a1fcb7f8578e4381b30ef38cdd
get-started-tdd <32414154@qq.com>
2015-06-05 19:36:05 +0800

    ^roman number doesn't have 0

--- src/test/java/test/com/getstartedtdd/katas/RomanNumberTest.java
+++ src/test/java/test/com/getstartedtdd/katas/RomanNumberTest.java
@@ -1,7 +1,17 @@
 package test.com.getstartedtdd.katas;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by L.x on 15-6-5.
  */
 public class RomanNumberTest {
+
+    @Test
+    public void roman_number_does_not_have_0() throws Exception {
+        assertThat(new RomanNumber(0).toString(), is(""));
+    }
 }
