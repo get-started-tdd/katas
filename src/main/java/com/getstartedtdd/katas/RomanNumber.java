eb8e671488ed20cdb1bff2c20ad83fd484ca9d84
get-started-tdd <32414154@qq.com>
2015-06-05 19:41:00 +0800

    $convert(1)=="I" => by add if statement

--- src/main/java/com/getstartedtdd/katas/RomanNumber.java
+++ src/main/java/com/getstartedtdd/katas/RomanNumber.java
@@ -4,11 +4,17 @@ package com.getstartedtdd.katas;
  * Created by L.x on 15-6-5.
  */
 public class RomanNumber {
+    private int number;
+
     public RomanNumber(int number) {
+        this.number = number;
     }
 
     @Override
     public String toString() {
+        if (number == 1) {
+            return "I";
+        }
         return "";
     }
 }
