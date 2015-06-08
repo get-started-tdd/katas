ca4af4bc6f279b070dac43f498162adacf523349
get-started-tdd <32414154@qq.com>
2015-06-08 19:05:35 +0800

    $ add another test time containing seconds only

--- src/main/java/com/getstartedtdd/katas/Time.java
+++ src/main/java/com/getstartedtdd/katas/Time.java
@@ -5,10 +5,13 @@ package com.getstartedtdd.katas;
  * Created by L.x on 15-6-8.
  */
 public class Time {
+    private String time;
+
     public Time(String time) {
+        this.time = time;
     }
 
     public int seconds() {
-        return 17;
+        return Integer.parseInt(time);
     }
 }
