e0129d822da6e620b2313a5cc64a46910b1f4a05
get-started-tdd <32414154@qq.com>
2015-06-08 19:02:34 +0800

    ^ time containing seconds only

--- src/test/java/test/com/getstartedtdd/katas/TimeTest.java
+++ src/test/java/test/com/getstartedtdd/katas/TimeTest.java
@@ -1,7 +1,17 @@
 package test.com.getstartedtdd.katas;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by L.x on 15-6-8.
  */
 public class TimeTest {
+
+    @Test
+    public void containingSecondsOnly() throws Exception {
+        assertThat(new Time("17").seconds(), equalTo(17));
+    }
 }
