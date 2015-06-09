84e576d9d006dbf465e24b0c89ac5043fdbf9478
get-started-tdd <32414154@qq.com>
2015-06-10 02:01:42 +0800

    $ eval a literal => add obvious implementation

--- src/main/java/com/getstartedtdd/katas/Literal.java
+++ src/main/java/com/getstartedtdd/katas/Literal.java
@@ -6,11 +6,14 @@ import java.util.Map;
  * Created by L.x on 15-6-10.
  */
 public class Literal {
+    private String text;
+
     public Literal(String text) {
+        this.text = text;
     }
 
     public String eval(Map<String, Object> context) {
-        return null;
+        return text;
     }
 
 }
