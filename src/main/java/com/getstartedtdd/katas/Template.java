5018cd5d7123b51c9ae17ece2a3a4c00e56457ad
get-started-tdd <32414154@qq.com>
2015-06-10 02:30:47 +0800

    using variable `nodeTree` to collect nodes

--- src/main/java/com/getstartedtdd/katas/Template.java
+++ src/main/java/com/getstartedtdd/katas/Template.java
@@ -1,5 +1,7 @@
 package com.getstartedtdd.katas;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 
@@ -16,15 +18,20 @@ public class Template {
     public String eval(Map<String, Object> context) {
         Matcher matcher = Variable.VARIABLE_PATTERN.matcher(template);
         StringBuilder result = new StringBuilder();
+        List<Node> nodeTree = new ArrayList<Node>();
         int pos = 0;
         while (matcher.find()) {
             String expression = matcher.group();
             Literal literal = new Literal(template.substring(pos, matcher.start()));
-            result.append(literal.eval(context))
-                    .append(new Variable(expression).eval(context));
+            Variable variable = new Variable(expression);
+            nodeTree.add(literal);
+            nodeTree.add(variable);
             pos = matcher.end();
+            result.append(literal.eval(context));
+            result.append(variable.eval(context));
         }
         Literal literal = new Literal(template.substring(pos));
+        nodeTree.add(literal);
         result.append(literal.eval(context));
         return result.toString();
     }
