02299aa93ab4222d630804bbf3e5fea57430121d
get-started-tdd <32414154@qq.com>
2015-06-10 01:44:25 +0800

    refactoring => simplify code

--- src/main/java/com/getstartedtdd/katas/Template.java
+++ src/main/java/com/getstartedtdd/katas/Template.java
@@ -4,10 +4,14 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.quote;
+
 /**
  * Created by L.x on 15-6-10.
  */
 public class Template {
+    public static final String EXPRESSION_START = "${";
+    public static final String EXPRESSION_END = "}";
     private String template;
 
     public Template(String template) {
@@ -15,22 +19,30 @@ public class Template {
     }
 
     public String eval(Map<String, Object> context) {
-        Pattern pattern = Pattern.compile("\\$\\{(.*)\\}");
+        Pattern pattern = Pattern.compile(quote(EXPRESSION_START) + ".*" + quote(EXPRESSION_END));
         Matcher matcher = pattern.matcher(template);
         StringBuilder result = new StringBuilder();
+        int pos = 0;
         if (matcher.find()) {
-            String name = matcher.group(1);
-            String left = template.substring(0, matcher.start());
-            String right = template.substring(matcher.end());
-            result.append(left);
-            if (context.containsKey(name)) {
-                result.append(String.valueOf(context.get(name))).append(right);
-            } else {
-                result.append(matcher.group()).append(right);
-            }
-        } else {
-            result.append(template);
+            String expression = matcher.group();
+            result.append(template.substring(0, matcher.start()))
+                    .append(evalVariable(expression, context));
+            pos = matcher.end();
         }
+        result.append(template.substring(pos));
         return result.toString();
+    }
+
+    private String evalVariable(String expression, Map<String, Object> context) {
+        String name = nameOf(expression);
+        if (context.containsKey(name)) {
+            return String.valueOf(context.get(name));
+        } else {
+            return expression;
+        }
+    }
+
+    private String nameOf(String expression) {
+        return expression.substring(EXPRESSION_START.length(), expression.length() - EXPRESSION_END.length());
     }
 }
