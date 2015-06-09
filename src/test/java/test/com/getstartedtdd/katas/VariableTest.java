989150931511efad022f29acd316055673343f14
get-started-tdd <32414154@qq.com>
2015-06-10 02:08:34 +0800

    ^evaluate variable in context map

---
+++ src/test/java/test/com/getstartedtdd/katas/VariableTest.java
@@ -0,0 +1,23 @@
+package test.com.getstartedtdd.katas;
+
+import com.getstartedtdd.katas.Literal;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 15-6-10.
+ */
+public class VariableTest {
+    @Test
+    public void evaluate() throws Exception {
+        Map<String, Object> context = Collections.<String, Object>singletonMap("foo", "bar");
+        assertThat(new Variable("${foo}").eval(context), equalTo("bar"));
+    }
+}
