e83ef3896ba3a0a47a2f4e0efa34828d4f584837
get-started-tdd <32414154@qq.com>
2015-06-10 00:33:46 +0800

    ^ eval 'text' == 'text'

--- src/test/java/test/com/getstartedtdd/katas/TemplateTest.java
+++ src/test/java/test/com/getstartedtdd/katas/TemplateTest.java
@@ -1,8 +1,20 @@
 package test.com.getstartedtdd.katas;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by L.x on 15-6-10.
  */
 public class TemplateTest {
-
+    @Test
+    public void plainText() throws Exception {
+        Map<String, Object> context=new HashMap<String, Object>();
+        assertThat(new Template("text").eval(context), equalTo("text"));
+    }
 }
