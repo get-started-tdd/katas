5c11b415d13188052b4c1e132eaa7f7dc1128819
get-started-tdd <32414154@qq.com>
2015-06-10 01:59:15 +0800

    ^ eval a literal

---
+++ src/test/java/test/com/getstartedtdd/katas/LiteralTest.java
@@ -0,0 +1,20 @@
+package test.com.getstartedtdd.katas;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 15-6-10.
+ */
+public class LiteralTest {
+    private static final Map<String, Object> CONTEXT_UNUSED = null;
+
+    @Test
+    public void evaluate() throws Exception {
+        assertThat(new Literal("text").eval(CONTEXT_UNUSED),equalTo("text"));
+    }
+}
